3651a81417a66cda837fe6a16d2f3dfe9cfaa774
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2025-10-02 15:44:42 +0530

    Add app7-student-college-data-scalable contents

--- app1/main.jsx
+++
@@ -1,9 +0,0 @@
-// src/main.jsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Call from './App'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Call/>
-  </StrictMode>
-)
