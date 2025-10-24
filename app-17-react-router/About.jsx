280ebe7018f57c89377bb6131c9858396443c63d
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2025-10-24 17:21:16 +0530

    Add files via upload

---
+++ app-17-react-router/About.jsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>About Us</h1>
+      <p>
+        This project demonstrates how to set up navigation in React using
+        separate component files with React Router.
+      </p>
+    </div>
+  );
+}
