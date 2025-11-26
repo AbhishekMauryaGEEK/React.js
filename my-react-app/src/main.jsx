639f5b0c2fca037bae8231dbd29bfbcace15d57b
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2025-11-26 12:57:53 +0530

    Remove node_modules from tracking

---
+++ my-react-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import App from './app';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
